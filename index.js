98a405f48770d3a5113f8575205eaa02e256b555
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-07-18 18:04:54 +0500

    add Image component

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Image from './src/components/Image';
 
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(
-    <App />,
+    <Image
+        src='https://images-na.ssl-images-amazon.com/images/I/51%2BnPh0o8rL._SS160_.jpg'
+        width='100'
+        height='100'
+        alt='XFX RS Black RX 570 OC 4GB'
+    />,
     document.getElementById('root')
 );
