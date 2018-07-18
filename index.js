a64a53ed34eaebddc7ceabc6755938a4ae7a1b31
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-07-18 18:06:09 +0500

    add TextBox component

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Image from './src/components/Image';
+import TextBox from './src/components/TextBox';
 
 
 ReactDOM.render(
-    <Image
-        src='https://images-na.ssl-images-amazon.com/images/I/51%2BnPh0o8rL._SS160_.jpg'
-        width='100'
-        height='100'
-        alt='XFX RS Black RX 570 OC 4GB'
-    />,
+    <TextBox>String example</TextBox>,
     document.getElementById('root')
 );
