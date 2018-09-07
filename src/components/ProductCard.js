c1b5ef630cd380a7a54dd1e5760c43d5495776ac
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2018-09-07 17:47:44 +0500

    add cart button in product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Image from './Image';
 import TextBox from './TextBox';
 import Price from './Price';
+import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
 class ProductCard extends Component {
     render() {
@@ -13,6 +16,10 @@ class ProductCard extends Component {
                 </TextBox>
                 <Image src={item.imageUrl} width='100' height='100' alt={item.title} />
                 <Price price={item.price}/>
+                <Button color='success'>
+                    <span>В корзину</span>
+                    <FontAwesomeIcon icon={ faCartPlus }  size="1x"/>
+                </Button>
             </div>
         )
     }
